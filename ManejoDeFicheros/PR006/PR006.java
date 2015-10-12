efbb8ef6be66bf77f89a29f0956a344364060812
Afens <sheith.92@gmail.com>
2015-10-12 19:46:34 +0200

    PR006

--- ManejoDeFicheros/PR006/PR006.java
+++ ManejoDeFicheros/PR006/PR006.java
@@ -1,54 +1,59 @@
 import java.io.*;
+import java.util.Arrays;
 
 /**
  * Created by Afens on 12/10/2015.
  */
 public class PR006 {
     public static void main(String[] args) {
-        File fCopiar = new File("./fCopiar.txt"),
-                f1 = new File("./f1.txt"),
-                f2 = new File("./f2.txt"),
-                f3 = new File("./f3.txt"),
-                reconstruido = new File("reconstruido.txt");
-        FileWriter wriF1, wriF2, wriF3, wriRec;
-        FileReader lectorFCop, lectorF1, lectorF2, lectorF3;
-        char[] caracteres = new char[15], recuperarF1 = new char[5], recuperarF2 = new char[5] ,recuperarF3 = new char[5];
-        int i = 0 , j = 0, k = 0;
+        File origen = new File("PR006\\fichero.txt");
+        File unido = new File("PR006\\unido.txt");
+        BufferedReader reader, readerF1, readerF2, readerF3;
+        BufferedWriter writerF1, writerF2, writerF3, writerUnido;
+        char[] array = new char[15];
+
         try {
-            wriF1 = new FileWriter(f1);
-            wriF2 = new FileWriter(f2);
-            wriF3 = new FileWriter(f3);
-            wriRec = new FileWriter(reconstruido);
-            try {
-                lectorFCop = new FileReader(fCopiar);
-                while (lectorFCop.read(caracteres) != -1){
-                    wriF1.write(caracteres,0,5);
-                    wriF2.write(caracteres,5,5);
-                    wriF3.write(caracteres,10,5);
-                }
-                lectorF1 = new FileReader(f1);
-                lectorF2 = new FileReader(f2);
-                lectorF3 = new FileReader(f3);
-                wriF1.close(); wriF2.close(); wriF3.close();
-                while(i != -1 || j != -1 || k != -1){
-                    if (i != -1){
-                        if (i != 0)
-                            wriRec.write(recuperarF1);
-                        i = lectorF1.read(recuperarF1);
-                    }
-                    if (j != -1){
-                        if (j != 0)
-                            wriRec.write(recuperarF2);
-                        j = lectorF2.read(recuperarF2);
-                    }
-                    if (k != -1){
-                        if (k != 0)
-                            wriRec.write(recuperarF3);
-                        k = lectorF3.read(recuperarF3);
-                    }
-                }
-                lectorFCop.close(); lectorF1.close(); lectorF2.close(); lectorF3.close(); wriRec.close();
-            } catch (FileNotFoundException e) {}
-        } catch (IOException e1){}
+            reader = new BufferedReader(new FileReader(origen));
+            writerUnido = new BufferedWriter(new FileWriter(unido));
+
+            writerF1 = new BufferedWriter(new FileWriter("PR006\\f1.txt"));
+            writerF2 = new BufferedWriter(new FileWriter("PR006\\f2.txt"));
+            writerF3 = new BufferedWriter(new FileWriter("PR006\\f3.txt"));
+
+            while (reader.read(array) != -1) {
+                writerF1.write(array, 0, 5);
+                writerF2.write(array, 5, 5);
+                writerF3.write(array, 10, 5);
+                Arrays.fill(array, ' ');
+
+            }
+            writerF1.close();
+            writerF2.close();
+            writerF3.close();
+            reader.close();
+
+            readerF1 = new BufferedReader(new FileReader("PR006\\f1.txt"));
+            readerF2 = new BufferedReader(new FileReader("PR006\\f2.txt"));
+            readerF3 = new BufferedReader(new FileReader("PR006\\f3.txt"));
+
+            while (readerF1.read(array,0,5) != -1) {
+                readerF2.read(array, 5, 5);
+                readerF3.read(array, 10, 5);
+
+                writerUnido.write(array);
+
+
+            }
+            writerUnido.close();
+            readerF1.close();
+            readerF2.close();
+            readerF3.close();
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error en el E/S.");
+        }
     }
 }
