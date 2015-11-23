737b9f1e0252db3adc68cd702112307684554661
Afens <sheith.92@gmail.com>
2015-11-23 14:20:28 +0100

    23/11

--- ManejoDeConectores/PR012/PR012.java
+++ ManejoDeConectores/PR012/PR012.java
@@ -11,23 +11,30 @@ public class PR012 {
     public static void main(String[] args) {
         try {
             Connection connection = CargarConexion.cargar();
-            String sql = "SELECT CodAsignatura FROM horario where codOE=? and CodCurso=? and CodAsignatura not Like '@%' and CodTramo = ?;";
+            String sql = "SELECT codAsignatura from horario where codOe = ? and CodCurso = ? and codAsignatura not like '@%' " +
+                    "order by substring(codTramo, 2), substring(codTramo, 1) like 'L%' desc, substring(codTramo, 1) like 'M%' desc, " +
+                    "substring(codTramo, 1) like 'X%' desc, substring(codTramo, 1) like 'J%' desc;";
+            String codCurso;
             PreparedStatement statement = connection.prepareStatement(sql);
             ResultSet rs;
 
             System.out.println("Introduce codOe");
             statement.setString(1, Teclado.cadena());
             System.out.println("Introduce codCurso");
-            statement.setString(2, Teclado.cadena());
-            String dias="LMXJV";
-            System.out.printf("\t%-9s%-9s%-12s%-10s%-10s\n","Lunes","Martes","Miercoles","Jueves","Viernes");
+            codCurso = Teclado.cadena();
+
+            if (!codCurso.matches("\\dA"))
+                codCurso+="A";
+
+            statement.setString(2, codCurso);
+            rs = statement.executeQuery();
+
+            System.out.printf("\t%-9s%-9s%-12s%-10s%-10s\n", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes");
             for (int i = 1; i <= 6; i++) {
-                System.out.printf("%dª\t",i);
-                for (int j = 0; j < 5; j++) {
-                    statement.setString(3, String.valueOf(dias.charAt(j))+i);
-                    rs=statement.executeQuery();
-                    rs.next();
-                    System.out.printf("%-10s",rs.getString(1));
+                System.out.printf("%dª\t", i);
+                for (int j = 0; j < 5 && rs.next(); j++) {
+
+                    System.out.printf("%-10s", rs.getString(1));
                 }
                 System.out.println();
             }
